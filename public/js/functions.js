ebc95c2235be907b0d72f691030431107904ab2d
Carlos Dominguez <carlos.cfdr86@gmail.com>
2020-02-10 13:40:12 -0600

    Se agregaron los botones de editar y eliminar

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,22 +1,33 @@
-
-function GuardarDoctor(){
+function getDoctorFormData(){
     const doctor_name = document.querySelector('input[name="doctor_name"]').value
     const doctor_cedula = document.querySelector('input[name="doctor_cedula"]').value
     const doctor_especialidad = document.querySelector('input[name="doctor_especialidad"]').value
     const doctor_email = document.querySelector('input[name="doctor_email"]').value
 
+    return {
+            doctor_name,
+            doctor_cedula,
+            doctor_especialidad,
+            doctor_email
+        }
+}
 
-    console.log({
-        doctor_name,
-doctor_cedula,
-doctor_especialidad,
-doctor_email
-    })
 
-    axios.post('/doctores', {
-        doctor_name,
-doctor_cedula,
-doctor_especialidad,
-doctor_email
-    }).then(res => alert(res.data.message)).catch(err => console.log(err))
+function GuardarDoctor(){
+    
+
+    axios.post('/doctores', getDoctorFormData()).then(res => alert(res.data.message)).catch(err => console.log(err))
+}
+
+function EditarDoctor(id){
+    axios.put('/doctores/'+id, getDoctorFormData())
+}
+function EliminarDoctor(id){
+    axios.put('/doctores/'+id)
+}
+
+function ShowDoctor(id){
+    axios.get('/doctores/'+id)
+    .then(data => console.log(data.data))
+    .catch(err => console.log(err))
 }
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 const router = require('express').Router()
+const Doctor = require('../models/Doctor')
 
 router.get('/', (req,res) => res.render('index'))
-router.get('/get/doctorForm', (req,res) => res.render('register/doctor'))
+router.get('/get/doctorForm', async (req,res) => {
+    const doctores = await Doctor.find()
+    console.log(doctores);
+    
+
+    res.render('register/doctor', {doctores})
+})
 router.get('/get/employeesForm', (req,res) => res.render('register/employess'))
 
 
