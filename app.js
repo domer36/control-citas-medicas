69d95d0cbaa4d349cdea4ce214286caafc96c013
Carlos Dominguez <carlos.cfdr86@gmail.com>
2020-02-10 09:03:10 -0600

    Se termino de configurar el servidor añadiendo soporte para mongodv

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-const express = require('express')
+require('dotenv').config();
+
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express      = require('express');
+const hbs          = require('hbs');
+const mongoose     = require('mongoose');
+const logger       = require('morgan');
+const path         = require('path');
+
+mongoose.connect(process.env.DB, {useUnifiedTopology: true, useNewUrlParser: true})
+.then(db => console.log( 'DB connected'))
+.catch(err => console.log(console.error(err)))
+
 const app = express()
 
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, ()=> console.log('Server ready on http://localhost:3000'))
