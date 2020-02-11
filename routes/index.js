c9d210b4c8103d6152d6bf11d53dfadaaa795195
Carlos Dominguez <carlos.cfdr86@gmail.com>
2020-02-10 18:39:55 -0600

    Se agregó el formulario para los pacientes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const Doctor = require('../models/Doctor')
 const Recepcionista = require('../models/Recepcionista')
+const Pacientes = require('../models/Patient')
 
 router.get('/', (req,res) => res.render('index'))
 router.get('/get/doctorForm', async (req,res) => {
@@ -12,7 +13,11 @@ router.get('/get/recepcionistaForm', async (req,res) => {
     const recepcionistas = await Recepcionista.find()
     res.render('register/recepcionista', {recepcionistas})
 })
-router.get('/get/employeesForm', (req,res) => res.render('register/employess'))
+router.get('/get/pacienteForm', async (req,res) => {
+    const pacientes = await Pacientes.find()
+    console.log(pacientes)
+    res.render('register/paciente', {pacientes})
+})
 
 
 module.exports = router
