4d501986133cc6e38150dd5d8bc739e2acf470a5
MakiavelliPac <ridesburak@gmail.com>
2020-02-13 11:50:38 -0600

    Se termino el formato del pdf y se borraron las recetas creadas

--- controllers/creareceta.js
+++ controllers/creareceta.js
@@ -5,35 +5,53 @@ const fs =require("fs");
 exports.CrearPDF = (cita) => {
 
 
-var doc=new PDF();
+  var doc=new PDF();
 
-doc.pipe(fs.createWriteStream(`${__dirname}/../public/recetas/${cita._id}.pdf`));
-const Medico=cita.doctor.nombre
-const Especialidad=cita.especialidad.nombre
-const Cedula=cita.doctor.cedula
+  doc.pipe(fs.createWriteStream(`${__dirname}/../public/recetas/${cita._id}.pdf`));
+  const Paciente=cita.paciente.nombre
+  const Medico=cita.doctor.nombre
+  const Especialidad=cita.especialidad.nombre
+  const Cedula=cita.doctor.cedula
+  const Tratamiento=cita.tratamiento
+  const Diagnostico=cita.diagnostico
+  const Peso=cita.peso
+  const Estatura=cita.estatura
+  const Precion=cita.precion
+  const fecha=new Date()
 
-doc.fontSize(8);
-doc.text(`Nombre del Medico: ${Medico}
-Especialidad: ${Especialidad}
-Cedula Profecional: ${Cedula}`, {
-  align: 'left'
-});
+  doc.text(`Nombre del Paciente: ${Paciente}
+Peso: ${Peso}   Estatura: ${Estatura}   
+Preción: ${Precion}`, {
+    width:350,
+    align: 'left'
+  });
+  doc.text(`${fecha.getDate()}/ ${fecha.getMonth()+1}/ ${fecha.getFullYear()}`,0,60,{
+    align: 'right'
+  })
+  doc.image(__dirname + "/../public/images/midoc_total.png",420,80,{width:100})
+  doc.fontSize(10)
+  doc.text(`Diagnostico:
 
-doc.image(__dirname + "/../public/images/midoc_total.png",420,70,{width:100})
+${Diagnostico}`,75,135,{
+    width:440,
+    align:'justify'
+  })
+  doc.text(`Indicaciones Medicas:
 
-doc.moveDown();
-doc.moveDown();
-doc.moveDown();
-doc.text(`This text is justify.`, {
-  width: 440,
-  align: 'justify'
-});
-
-
-doc.rect(doc.x, 120, 450, doc.y).stroke();
-
-
-
-return doc.end()
-
+${Tratamiento}`,75,330, {
+    width: 440,
+    align: 'justify'
+  });
+  doc.rect(70, 130, 450, 180).stroke();
+  doc.rect(70, 320, 450, 180).stroke();
+  doc.rect(70, 600, 450, 0).stroke();
+  doc.fontSize(12)
+  doc.text(`
+  ${Medico}
+  ${Especialidad}
+  ${Cedula}`,75,600,{
+    width:450,
+    align:"center"
+  })
+  return doc.end()
 }
